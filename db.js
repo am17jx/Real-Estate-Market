18a1c94bdda2bae8800c20e6513c5ec1705c85da
ameer ahmed <136123824+am17jx@users.noreply.github.com>
2025-09-15 14:19:37 +0300

    Update db.js

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('ameer_db', 'postgres', 'newcore', {
+const sequelize = new Sequelize('ameer_db', 'postgres', 'password', {
   host: 'localhost',
   port: 5432,
   dialect: 'postgres',
